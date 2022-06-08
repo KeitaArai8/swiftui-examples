b286b48608663e613ed9b52343393f035d1e1c67
Keita Arai <ynwano.88@gmail.com>
2022-06-08 22:52:08 +0900

    add TextFieldKeyboardToolbarView

---
+++ Examples/TextFieldKeyboardToolbarView/TextFieldKeyboardToolbarView/TextFieldKeyboardToolbarViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldKeyboardToolbarViewApp.swift
+//  TextFieldKeyboardToolbarView
+//
+//  Created by Keita Arai on 2022/06/08.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldKeyboardToolbarViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
